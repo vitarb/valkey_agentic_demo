038e0334d0d6365059059dbd0e895a5c91aca12c
Vitaly <Vitaly.Arbuzov@gmail.com>
2025-05-29 15:28:02 -0700

    Trim feeds to configured length

--- fanout.lua
+++ fanout.lua
@@ -6,8 +6,9 @@ local key   = 'user:topic:'..ARGV[2]
 local users = redis.call('ZRANGE', key, 0, -1)   -- all user ids for topic
 
 for _,u in ipairs(users) do
-  redis.call('LPUSH', 'feed:'..u, ARGV[3])
-  redis.call('LTRIM', 'feed:'..u, 0, tonumber(ARGV[4]) - 1)
+  local feed_key = 'feed:'..u
+  redis.call('LPUSH', feed_key, ARGV[3])
+  redis.call('LTRIM', feed_key, 0, tonumber(ARGV[4]) - 1)
 end
 return #users
 
